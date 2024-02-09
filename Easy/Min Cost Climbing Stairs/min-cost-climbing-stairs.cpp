fcbad3689ad00bc738c8fc399de1dd59e0814dc6
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-02-10 04:02:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Min Cost Climbing Stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    int minCostClimbingStairs(vector<int>&cost ,int N) {
+        int dp[N+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2; i<N; i++){
+            dp[i]=min(dp[i-1],dp[i-2])+cost[i];
+        }
+        dp[N]=min(dp[N-1], dp[N-2]);
+        return dp[N];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        vector<int>cost(N);
+        
+        for(int i=0 ; i<N ; i++){
+            cin>>cost[i];
+        }
+
+        Solution ob;
+        cout<<ob.minCostClimbingStairs(cost,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

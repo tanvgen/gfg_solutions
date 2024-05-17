a066e4a2fed0d30851050f09edc00017dd336510
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-05-18 01:48:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -10,65 +10,27 @@ using namespace std;
 class Solution{   
 public:
     bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
         int n=arr.size();
-        int k=sum;
-        vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
-
-    // Base case: If the target sum is 0, we can always achieve it by taking no elements
-    for (int i = 0; i < n; i++) {
-        dp[i][0] = true;
-    }
-
-    // Base case: If the first element of 'arr' is less than or equal to 'k', set dp[0][arr[0]] to true
-    if (arr[0] <= k) {
-        dp[0][arr[0]] = true;
-    }
-
-    // Fill the DP array iteratively
-    for (int ind = 1; ind < n; ind++) {
-        for (int target = 1; target <= k; target++) {
-            // If we don't take the current element, the result is the same as the previous row
-            bool notTaken = dp[ind - 1][target];
-
-            // If we take the current element, subtract its value from the target and check the previous row
-            bool taken = false;
-            if (arr[ind] <= target) {
-                taken = dp[ind - 1][target - arr[ind]];
-            }
-
-            // Store the result in the DP array for the current subproblem
-            dp[ind][target] = notTaken || taken;
+        vector<vector<int>> dp(n, vector<int> (sum+1,0));
+        
+        for(int i=0; i<n; i++){
+            dp[i][0]=1;
         }
-    }
-
-    // The final result is stored in dp[n-1][k]
-    return dp[n - 1][k];
-    //     bool dp[n][sum+1];
-    //     for(int i=0; i<n; i++){
-    //         for(int j=0; j<=sum; j++){
-    //             dp[i][j]=false;
-    //         }
-    //     }
-    //   // vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
-        
-    //     for(int i=0; i<n; i++){
-    //         dp[i][0]=true;
-    //     }
-    //     if(arr[0]<=sum) {
-    //         dp[0][arr[0]]=true;
-    //     }
-        
-    //     for(int i=1; i<n; i++){
-    //         for(int j=1; j<=sum; j++){
-    //             bool nottaken=dp[i-1][j];
-    //             bool taken=false;
-    //             if(arr[i]<=sum){
-    //                 taken=dp[i-1][j-arr[i]];
-    //             }
-    //             dp[i][j]=nottaken||taken;
-    //         }
-    //     }
-    //     return dp[n-1][sum];
+        if(arr[0]<=sum){
+            dp[0][arr[0]]=1;
+        }
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=sum; j++){
+                bool ntake=dp[i-1][j];
+                bool take=false;
+                if(j>=arr[i]){
+                    take=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=take||ntake;
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 

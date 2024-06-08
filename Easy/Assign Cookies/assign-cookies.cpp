e550e5680d62461d664c071b4fe9f93ef3ebd0e8
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-06-08 23:36:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Assign Cookies/assign-cookies.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxChildren(int N, int M, vector<int> &greed, vector<int> &sz) {
+        // code here
+        int ans=0;
+        sort(greed.begin(), greed.end());
+        sort(sz.begin(), sz.end());
+        int i=0,j=0;
+        
+        while(i<N && j<M){
+           if(sz[j] >= greed[i]){
+                i++;
+            }
+            
+            j++;
+        }
+        return i;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<int> greed(N), sz(M);
+        for (int i = 0; i < N; i++) {
+            cin >> greed[i];
+        }
+        for (int i = 0; i < M; i++) {
+            cin >> sz[i];
+        }
+        Solution obj;
+        cout << obj.maxChildren(N, M, greed, sz) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

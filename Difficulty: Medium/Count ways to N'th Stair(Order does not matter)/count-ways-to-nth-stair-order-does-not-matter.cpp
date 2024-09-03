02e7dd69eabf93c2d5cee4951c95a3a1f773fd0b
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-09-03 11:06:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count ways to N'th Stair(Order does not matter)/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	    int solve(int n, vector<int> &dp){
+	        if(n==0) return 1;
+	        if(dp[n]!=-1) return dp[n];
+	        
+	        return dp[n]=1+min(solve(n-1,dp),solve(n-2,dp));
+	    }
+		int nthStair(int n){
+		    //  Code here
+		    vector<int> dp(n+1,-1);
+		    dp[1]=1;
+		    dp[2]=2;
+		    
+		    return solve(n,dp);
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthStair(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends

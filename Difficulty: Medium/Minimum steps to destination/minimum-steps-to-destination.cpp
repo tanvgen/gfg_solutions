6b7192b6cb5a1bc6de3e264c14ffc1246c971aee
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-09-03 06:22:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum steps to destination/minimum-steps-to-destination.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d) {
+        // code here
+        for(int i=0; i<=2*d; i++){
+            int sum=i*(i+1)/2;
+            if((sum+d)%2==0 && sum>=d){
+                return i;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int d;
+        cin >> d;
+
+        Solution ob;
+        cout << ob.minSteps(d) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

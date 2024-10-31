bf00b87b73e87655d4b36ecd7fdef70677c33d8e
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-10-31 18:42:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ Difficulty: Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,19 +1,82 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
 
 
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int findCity(int n, int m, vector<vector<int>>& edges, int distanceThreshold) {
+        // Your code here
+        vector<vector<int>> dis(n,vector<int>(n,1e9));
+        
+        for(int i=0; i<n; i++) dis[i][i]=0;
+        
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            dis[u][v]=wt;
+            dis[v][u]=wt;
+        }
+        
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
+                }
+            }
+        }
+        
+        vector<int> ct(n,0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i!=j && dis[i][j]<=distanceThreshold){
+                    ct[i]++;
+                    ct[j]++;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0; i<n; i++){
+            if(ct[ans]>=ct[i]){
+                ans=i;
+            }
+        }
+        return ans;
+    }
+};
 
 
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> adj;
+        // n--;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
 
+        int dist;
+        cin >> dist;
+        Solution obj;
+        cout << obj.findCity(n, m, adj, dist) << "\n";
+    
+cout << "~" << "\n";
+}
+}
 
-
-
-
-class Solution {      int findCity(int n, int m, int[][] edges,int distanceThreshold)      {          //code here         int[][] dist=new int[n][n];        for(int i=0;i<n;i++){            for(int j=0;j<n;j++) dist[i][j]=Integer.MAX_VALUE;        }        for(int i=0;i<edges.length;i++){            dist[edges[i][0]][edges[i][1]]=edges[i][2];            dist[edges[i][1]][edges[i][0]]=edges[i][2];        }
-        for(int i=0;i<n;i++) dist[i][i]=0;        for(int k=0;k<n;k++){            for(int i=0;i<n;i++){                for(int j=0;j<n;j++){                    if(dist[i][k]==Integer.MAX_VALUE||dist[k][j]==Integer.MAX_VALUE) continue;                    dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);                }            }        }        int cnt=n;        int ans=-1;        for(int i=0;i<n;i++){            int c=0;            for(int j=0;j<n;j++){                if(dist[i][j]<=distanceThreshold) c++;            }            if(c<=cnt){                cnt=c;                ans=i;            }        }        return ans;      }}
-
-
-
-
-
-
-
-
+// } Driver Code Ends

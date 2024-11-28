c77c894920e7c164583de7ef0b0d2d1dcd3dfc38
tanvgen <124526871+tanvgen@users.noreply.github.com>
2024-11-28 22:30:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -4,12 +4,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
 class Solution{
 public:
-    int solve(int i, int j, int N, int W, int val[], int wt[], vector<vector<int>> &dp){
+    int solve(int i, int j, int N, int W, vector<int> &val, vector<int> &wt, vector<vector<int>> &dp){
         // if(i==N){
         //     if(j==W) return dp[i][j];
         //     else return 0;
@@ -25,33 +26,45 @@ public:
         
         return dp[i][j]=max(take,ntake);
     }
-    int knapSack(int N, int W, int val[], int wt[])
+    int knapSack(vector<int> &val, vector<int> &wt, int W)
     {
         // code here
+        int N=val.size();
         vector<vector<int>> dp(N,vector<int> (W+1,-1));
         return solve(0,0,N,W,val,wt,dp);
     }
 };
 
+
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int N, W;
-        cin>>N>>W;
-        int val[N], wt[N];
-        for(int i = 0;i < N;i++)
-            cin>>val[i];
-        for(int i = 0;i < N;i++)
-            cin>>wt[i];
-        
+    cin >> t;
+    while (t--) {
+        int W;
+        cin >> W;
+        cin.ignore();
+        string str;
+        getline(cin, str);
+        stringstream ss(str);
+        vector<int> val;
+        int num;
+        while (ss >> num) {
+            val.push_back(num);
+        }
+        string str2;
+        getline(cin, str2);
+        stringstream ss2(str2);
+        vector<int> wt;
+        int num2;
+        while (ss2 >> num2) {
+            wt.push_back(num2);
+        }
         Solution ob;
-        cout<<ob.knapSack(N, W, val, wt)<<endl;
-    
-cout << "~" << "\n";
-}
+        cout << ob.knapSack(val, wt, W) << endl;
+        cout << "~" << endl;
+    }
     return 0;
 }
 // } Driver Code Ends

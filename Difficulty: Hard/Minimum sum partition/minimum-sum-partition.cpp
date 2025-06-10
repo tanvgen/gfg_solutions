7603f2778c1fa7e724997118ca899f1facf2b6ef
tanvgen <124526871+tanvgen@users.noreply.github.com>
2025-06-10 15:59:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -1,66 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution{
+class Solution {
 
   public:
-  
-  vector<vector<int>> dp;
-    int solve(int i , int sum , int rs ,int n ,  int arr[]){
-        
-        if(i >= n){
+    int solve(int i, int currsum, int n, int tsum, vector<int> &arr, vector<vector<int>> &dp){
+        if(i>=n){
+            int s1=currsum;
+            int s2=tsum-currsum;
             
-            int s1 = sum;
-            int s2 = rs -sum;
-            
-            return abs(s1 -s2);
-            
+            return abs(s1-s2);
         }
-        if(dp[i][sum] != -1){
-            return dp[i][sum];
-            
-        }
-        int take = solve(i+1 ,sum+ arr[i] , rs ,n, arr);
-        int nontake = solve(i +1 , sum , rs , n,arr);
+        if(dp[i][currsum]!=-1) return dp[i][currsum];
         
-        return dp[i][sum]= min(take  , nontake);
+        int take=solve(i+1,currsum+arr[i],n,tsum,arr,dp);
+        int ntake=solve(i+1,currsum,n,tsum,arr,dp);
         
+        return dp[i][currsum]=min(take,ntake);
     }
-	int minDifference(int arr[], int n)  { 
-	    // Your code goes here
-	    int sum = accumulate(arr , arr+n ,0);
-        dp.resize(n , vector<int> (sum , -1));
-        return solve(0 , 0, sum ,n , arr);
-	} 
+    int minDifference(vector<int>& arr) {
+        // Your code goes here
+        int n=arr.size();
+        int tsum=0;
+        for(int i=0; i<n; i++) tsum+=arr[i];
+        
+        vector<vector<int>> dp(n,vector<int>(tsum+1,-1));
+        
+        return solve(0,0,n,tsum,arr,dp);
+    }
 };
-
-
-//{ Driver Code Starts.
-int main() 
-{
-   
-   
-   	int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-
-        int a[n];
-        for(int i = 0; i < n; i++)
-        	cin >> a[i];
-
-       
-
-	    Solution ob;
-	    cout << ob.minDifference(a, n) << "\n";
-	     
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
